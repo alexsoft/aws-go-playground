4a5ed042c392b3d831c4a49d379b54f11a6ab84d
Oleksii Plekhanov <1451894+alexsoft@users.noreply.github.com>
2026-04-29 15:17:32 +0200

    Added more logs, extracted getAppServer

--- main.go
+++ main.go
@@ -12,6 +12,11 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
+
+	fmt.Println("aws-go-playground")
+	fmt.Printf("port = %s\n", port)
+	fmt.Printf("app-server = %s\n", getAppServer())
+
 	http.HandleFunc("/", index)
 	http.HandleFunc("/healthcheck", healthcheck)
 
@@ -23,10 +28,15 @@ func index(w http.ResponseWriter, req *http.Request) {
 }
 
 func healthcheck(w http.ResponseWriter, req *http.Request) {
+	server := getAppServer()
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]any{"ok": true, "app-server": server})
+}
+
+func getAppServer() string {
 	server := os.Getenv("APP_SERVER")
 	if server == "" {
 		server = "NA"
 	}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]any{"ok": true, "app-server": server})
+	return server
 }
